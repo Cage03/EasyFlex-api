331f740feb910b33d22e1e4b0d1fbeb8c8093a58
Max <maxvaskovich@hotmail.com>
2024-10-02 14:05:30 +0200

    Updated Models

--- Interface/Models/SkillModel.cs
+++ Interface/Models/SkillModel.cs
@@ -1,19 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Interface.Models;
 
 public partial class SkillModel
 {
     public int Id { get; set; }
 
+    [Required]
     public int CategoryId { get; set; }
 
+    [Required]
     public string Name { get; set; } = null!;
 
-    public virtual CategoryModel CategoryModel { get; set; } = null!;
+    [Required]
+    public virtual CategoryModel Category { get; set; } = null!;
 
     public virtual ICollection<FlexworkerModel> Flexworkers { get; set; } = new List<FlexworkerModel>();
-
     public virtual ICollection<JobModel> Jobs { get; set; } = new List<JobModel>();
-    
+
     // public SkillDto ToDto()
     // {
     //     return new SkillDto
