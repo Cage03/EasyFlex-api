ffadc0ee03b993c153412fdc937bd1ae0374b473
Max <maxvaskovich@hotmail.com>
2024-10-10 15:19:08 +0200

    create job
    changed return and made unit tests
    
    unit test formatting

--- Interface/Interface/Dal/IJobDal.cs
+++ Interface/Interface/Dal/IJobDal.cs
@@ -4,7 +4,7 @@ namespace Interface.Interface.Dal;
 
 public interface IJobDal
 {
-    public Task CreateJob(JobModel job);
+    public Task<int> CreateJob(JobModel job);
     public Task<JobModel?> GetJob(int id);
     public Task<List<JobModel>> GetJobs(int offset, int limit);
     public Task UpdateJob(JobModel job);
--- Test/UnitTests/Handlers/JobHandlerTests.cs
+++ Test/UnitTests/Handlers/JobHandlerTests.cs
@@ -65,5 +65,33 @@ namespace Test.UnitTests.Handlers
             // Assert
             Assert.AreEqual(0, result!.Length);
         }
+
+        [TestMethod]
+        public async Task CreateJob_ShouldReturn1IfSuccessful ()
+        {
+            //Arrange
+            JobModel job = new() { Id = 1, Name = "Job1", Address = "Address1", MinHours = 10, MaxHours = 20, StartDate = new DateOnly(2024, 10, 1) };
+            _mockJobDal.Setup(x => x.CreateJob(It.IsAny<JobModel>())).ReturnsAsync(1);
+
+            //Act
+            var result = await _jobHandler.CreateJob(job);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public async Task CreateJob_ShouldReturn0IfUnsuccessful()
+        {
+            //Arrange
+            JobModel job = new() { Id = 1, Name = "Job1", Address = "Address1", MinHours = 10, MaxHours = 20, StartDate = new DateOnly(2024, 10, 1) };
+            _mockJobDal.Setup(x => x.CreateJob(It.IsAny<JobModel>())).ReturnsAsync(0);
+
+            //Act
+            var result = await _jobHandler.CreateJob(job);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
